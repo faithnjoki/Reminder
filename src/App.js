fdb9ced148cab68210a1b88281143df4dec8e2a4
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-11-07 16:19:00 +0300

    create my components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import List from './List';
+import Data from './Data'
+import React, {useState} from 'react';
 
 function App() {
+  const [people,setPeople] = useState[Data]
   return (
     <div className="App">
-      he
+    <h3>List of birthdays</h3>
+    {/* <List/> */}
+    <button onclick={() =>console.log('ypu have been clicked')}>Clear all</button>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,22 @@
+export default[
+    {
+        id:1,
+        name: 'Leon Ronald',
+        age: 1,
+        image:'https://cdn.pixabay.com/photo/2014/07/11/22/05/baby-390555__480.jpg',
+    },
+    {
+        id:2,
+        name: 'Mary Jane',
+        age: 30,
+        image:'https://cdn.pixabay.com/photo/2017/01/20/18/25/woman-1995624__480.jpg',
+    },
+    {
+        id:3,
+        name: 'Jane Bosco',
+        age: 39,
+        image:'https://cdn.pixabay.com/photo/2020/07/30/10/29/woman-5450043__480.jpg'
+    },
+
+
+]
